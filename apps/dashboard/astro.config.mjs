ad78e653011ddc938e62743633a273e6eccfead1
Etherless <pabloimrik17@gmail.com>
2024-04-17 15:47:23 +0200

    added codecov plugin in astro project

--- apps/dashboard/astro.config.mjs
+++ apps/dashboard/astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      // Put the Codecov vite plugin after all other plugins
+      codecovVitePlugin({
+        enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+        bundleName: "@kinkoi/dashboard",
+        uploadToken: process.env.CODECOV_TOKEN,
+      }),
+    ]
+  },
+});
