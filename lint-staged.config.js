1e8f549bbbca8bfe2bbd6fce2b03a84a6c5875fa
Etherless <pabloimrik17@gmail.com>
2024-04-20 19:32:39 +0200

    chore: added prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,10 +3,11 @@ const ESLINT_MAX_WARNINGS = 10;
 
 module.exports = {
     "**/*": "prettier --write --ignore-unknown",
-    "*.{tsx,ts}": (fileNames) => [
-        `eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`,
-        // `prettier ${fileNames.join(" ")} --write --ignore-unknown`,
-    ],
+    "*.{tsx,ts}": [`eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`],
+    // "*.{tsx,ts}": (fileNames) => [
+    //     `eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`,
+    //     // `prettier ${fileNames.join(" ")} --write --ignore-unknown`,
+    // ],
     "*.css": "stylelint --fix",
     "*.scss": "stylelint --fix",
 };
