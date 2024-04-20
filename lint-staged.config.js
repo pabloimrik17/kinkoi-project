b088620b3851941c2cd108c63c5b2ad196a97848
Etherless <pabloimrik17@gmail.com>
2024-04-20 19:20:49 +0200

    chore: added lint-staged and pre-commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,10 @@
 const ESLINT_MAX_WARNINGS = 10;
 
 module.exports = {
-    // "*.{tsx,ts}": (fileNames) => [
-    //     `eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`,
-    //     `prettier ${fileNames.join(" ")} --write --ignore-unknown`,
-    // ],
-    // "*.css": "stylelint --fix",
-    // "*.scss": "stylelint --fix",
+    "*.{tsx,ts}": (fileNames) => [
+        `eslint --fix --max-warnings ${ESLINT_MAX_WARNINGS}`,
+        `prettier ${fileNames.join(" ")} --write --ignore-unknown`,
+    ],
+    "*.css": "stylelint --fix",
+    "*.scss": "stylelint --fix",
 };
