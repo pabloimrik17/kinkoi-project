5b2e10233e13945bd0e84a36db9edeb1caedc401
Pablo. Fernandez Guerra <pablo.fernandez@nazaries.com>
2024-04-23 13:02:52 +0200

    feat: added new endpoint

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  getGreetings(): string {
+    return this.appService.getGreetings();
+  }
 }
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getGreetings(): string {
+    return 'Hi guys!!';
+  }
 }
