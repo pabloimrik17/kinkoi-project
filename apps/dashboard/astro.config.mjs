c22be7e907f8a15d97ff2be0a973bea4f34c3120
Pablo F. Guerra <pabloimrik17@gmail.com>
2024-05-04 19:59:58 +0200

    feat: enabled ssr

--- apps/dashboard/astro.config.mjs
+++ apps/dashboard/astro.config.mjs
@@ -1,16 +1,22 @@
-import { defineConfig } from "astro/config";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [
-      // Put the Codecov vite plugin after all other plugins
-      codecovVitePlugin({
-        enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-        bundleName: "@kinkoi/dashboard",
-        uploadToken: process.env.CODECOV_TOKEN,
-      }),
-    ]
-  },
+    output: "server",
+    vite: {
+        plugins: [
+            // Put the Codecov vite plugin after all other plugins
+            codecovVitePlugin({
+                enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+                bundleName: "@kinkoi/dashboard",
+                uploadToken: process.env.CODECOV_TOKEN,
+            }),
+        ],
+    },
+    adapter: node({
+        mode: "standalone",
+    }),
 });
